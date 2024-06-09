e59df53675b34f56f8a8609e9f33d5c895501fff
hashmat1530 <hashmat2020vskp@gmail.com>
2024-06-09 13:37:25 +0530

    remove todo functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
 import './App.css'
 
 function App() {
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       Hustler
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
